cd8d555b20ae13334cf98e461c99fe2706d2bbf7
NaoufalD <36072212+NaoufalD@users.noreply.github.com>
2019-09-06 12:49:38 +0200

    Add files via upload

--- code.py
+++ code.py
@@ -5,10 +5,32 @@ def groet():
 for i in range(10):
     groet()
 """
-
+"""
+def vijf():
+       return 5
+  def maal_vijf(i):
+       return 5 * i
+"""
+"""
+def getal_maal_vijf(i):
+    getal = int(input("Voer een getal in: "))
+    resultaat = maal_vijf(getal)
+"""
+"""
+ print("getal maal 5 =" + str(resultaat))
+"""
+"""
 def tafel5():
     tafel = 0
     for i in range(10):
         tafel = tafel + 1
         print(tafel * 5)
 tafel5()
+""" 
+def groet(naam):
+  print("Hallo " + naam)
+        
+groet("Kees")
+groet("Loes")
+groet("Tom")
+groet("Anne")

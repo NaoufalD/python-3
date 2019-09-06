4266539bb46396f1f1b59fc3fc6bf3ed7a33e703
NaoufalD <36072212+NaoufalD@users.noreply.github.com>
2019-09-06 12:21:47 +0200

    Add files via upload

---
+++ code.py
@@ -0,0 +1,14 @@
+"""
+def groet():
+    print("hallo")
+   
+for i in range(10):
+    groet()
+"""
+
+def tafel5():
+    tafel = 0
+    for i in range(10):
+        tafel = tafel + 1
+        print(tafel * 5)
+tafel5()
